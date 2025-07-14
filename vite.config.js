e55751ad1958d4c4139267f27f737f0748b36c05
kanacyu99 <nakamuraya.ra.yu@gmail.com>
2025-07-14 15:11:58 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 4173
-  },
-  preview: {
-    port: 4173,
-    host: '0.0.0.0',
-    allowedHosts: ['sangenkei-20250714.onrender.com']
-  }
 })
