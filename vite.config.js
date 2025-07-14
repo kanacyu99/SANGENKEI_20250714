957ad2a7f347c8364f88f82d82bd8927c13a5b5d
kanacyu99 <nakamuraya.ra.yu@gmail.com>
2025-07-14 13:23:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 4173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['sangenkei-20250714.onrender.com']
   }
 })
